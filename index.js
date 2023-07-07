400de4b94802f47dec7f7d68e5b89329116c168f
Garrett-Bodley <garrett.bodley@gmail.com>
2023-07-07 17:55:17 -0400

    Change text indent.

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ const home = () => {
   for(let i = 0; i < 100; i++) console.log()
   console.log(figlet.textSync('Tic Tac Toe', {font: 'ANSI Shadow'}))
   console.log(`
-  ========================================================================================================
-  
-  ${chalk.bold.underline.yellow(`How to Play:`)}
-  ${chalk.bold.yellow(`1. The game is played on a grid that's 3 squares by 3 squares.`)}
-  ${chalk.bold.yellow(`2. The first player is known as X and the second is O. Players take turns putting their marks in empty squares.`)}
-  ${chalk.bold.yellow(`3. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.`)}
-  ${chalk.bold.yellow(`4. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.`)}
-  
+    ========================================================================================================
+    
+    ${chalk.bold.underline.yellow(`How to Play:`)}
+    ${chalk.bold.yellow(`1. The game is played on a grid that's 3 squares by 3 squares.`)}
+    ${chalk.bold.yellow(`2. The first player is known as X and the second is O. Players take turns putting their marks in empty squares.`)}
+    ${chalk.bold.yellow(`3. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.`)}
+    ${chalk.bold.yellow(`4. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.`)}
+    
 
-  ${chalk.bold.underline.bgYellowBright(`Press SPACE to START`)}
-  ========================================================================================================
-  `)
+    ${chalk.bold.underline.bgYellowBright(`Press SPACE to START`)}
+    ========================================================================================================`
+  )
   process.stdin.on('keypress', (str, key) => {
     if(key.sequence == ' ') play()
   })
