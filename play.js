270f3f41b3fea27c4c440a5c9bed2cf5c14ee789
Garrett-Bodley <garrett.bodley@gmail.com>
2023-07-07 15:17:16 -0400

    Make function to print board to console.

--- play.js
+++ play.js
@@ -6,6 +6,7 @@ export default function play (){
   const boardState = makeState()
   let selected = 0
   boardState[selected].isSelected = true
+  printBoard(boardState)
 
 }
 
@@ -15,4 +16,15 @@ const makeState = () => {
     output.push(new Space())
   }
   return output
+}
+
+const printBoard = (boardState) => {
+  const board = `
+${boardState[0].toString()}|${boardState[1].toString()}|${boardState[2].toString()}
+-----
+${boardState[3].toString()}|${boardState[4].toString()}|${boardState[5].toString()}
+-----
+${boardState[6].toString()}|${boardState[7].toString()}|${boardState[8].toString()}
+`
+  console.log(board)
 }
