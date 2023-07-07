833a99fa142588f829a1a3fafefd3904114f2500
Garrett-Bodley <garrett.bodley@gmail.com>
2023-07-07 13:32:01 -0400

    Start working on game logic in separate file.

---
+++ play.js
@@ -0,0 +1,17 @@
+export default function play (){
+
+const board = `
+ | | 
+-----
+ | | 
+-----
+ | | 
+`
+
+console.log(board)
+}
+
+// display the board
+// let user navigate board
+// allow user to make a move/claim space
+// 
